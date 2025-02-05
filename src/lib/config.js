dcc1e483017293025f4dfb31b6ec2ba7211aa991
@s.roertgen <sroertgen@gmail.com>
2025-02-05 18:41:49 +0100

    (ui) we talk about hours

--- src/lib/config.js
+++ src/lib/config.js
@@ -7,7 +7,7 @@ export const uriMappings = {
 	'https://w3id.org/lehrplan/ontology/LP_0002050': 'Inhalte zu den Kompetenzen (BY)',
 	'https://w3id.org/lehrplan/ontology/LP_0002043': 'Fachlehrplan (BY)',
 	'https://w3id.org/lehrplan/ontology/LP_0002046': 'Lernbereich (BY)',
-	'https://w3id.org/lehrplan/ontology/dauer': 'Dauer',
+	'https://w3id.org/lehrplan/ontology/dauer': 'Dauer (h)',
 	'https://w3id.org/lehrplan/ontology/hatSchulart': 'Schulart',
 	'https://w3id.org/lehrplan/ontology/hatJahrgangsstufe': 'Jahrgangsstufe',
 	'https://w3id.org/lehrplan/ontology/hatFach': 'Fach'
