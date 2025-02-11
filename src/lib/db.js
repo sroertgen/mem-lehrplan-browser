0a0664fd869163ed7fc669ca5d66145255f36790
@s.roertgen <sroertgen@gmail.com>
2025-02-11 15:48:02 +0100

    Move logic out of components

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,38 @@
 import { writable, get } from 'svelte/store';
 import { config } from '$lib/config';
+import { queryFTS } from '$lib/fts';
 
 export const db = writable({
-	selectedFilters: initFilters()
+	filters: initFilters(),
+	selectedFilters: initSelectedFilters(),
+	results: []
 });
 
 export function getDB() {
 	return get(db);
 }
 
-function initFilters() {
+async function initFilters() {
+	const faecherRaw = await fetch(`/api/getFilters?filter=${'fach'}`);
+	const faecherList = await faecherRaw.json();
+	const sortedFaecher = faecherList.sort((a, b) =>
+		a.fach.value.localeCompare(b.fach.value, 'de', { sensitivity: 'base' })
+	);
+	const faecher = ['fach', sortedFaecher];
+
+	const jahrgangsstufeRaw = await fetch(`/api/getFilters?filter=${'jahrgangsstufe'}`);
+	const jahrgangsstufeList = await jahrgangsstufeRaw.json();
+	const sortedJahrgangsstufe = jahrgangsstufeList.sort(
+		(a, b) => Number(a.jahrgangsstufe.value) - Number(b.jahrgangsstufe.value)
+	);
+
+	const jahrgangsstufe = ['jahrgangsstufe', sortedJahrgangsstufe];
+	db.update((db) => {
+		return { ...db, filters: [faecher, jahrgangsstufe] };
+	});
+}
+
+function initSelectedFilters() {
 	return Object.fromEntries(config.filterKeys.map((e) => [e, []]));
 }
 
@@ -29,4 +52,16 @@ export function toggleFilter(key, val) {
 		}
 		return { ...db, selectedFilters };
 	});
+}
+
+export async function handleQuery(searchTerm) {
+	try {
+		const res = await queryFTS(searchTerm);
+		const results = await res.json();
+		db.update((db) => {
+			return { ...db, results };
+		});
+	} catch (error) {
+		console.error('Error fetching results:', error);
+	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,31 @@
 <script>
-	import { onMount } from 'svelte';
+	import { db, handleQuery } from '$lib/db';
 	import Result from '$lib/components/Result.svelte';
 	import Filter from '$lib/components/Filter.svelte';
-	import { queryFTS } from '$lib/fts';
 
-	let search = '';
-	let results = [];
-	let filters = [];
-
-	async function loadFilters() {
-		const faecherRaw = await fetch(`/api/getFilters?filter=${'fach'}`);
-		const faecherList = await faecherRaw.json();
-		const sortedFaecher = faecherList.sort((a, b) =>
-			a.fach.value.localeCompare(b.fach.value, 'de', { sensitivity: 'base' })
-		);
-		const faecher = ['fach', sortedFaecher];
-
-		const jahrgangsstufeRaw = await fetch(`/api/getFilters?filter=${'jahrgangsstufe'}`);
-		const jahrgangsstufeList = await jahrgangsstufeRaw.json();
-		const sortedJahrgangsstufe = jahrgangsstufeList.sort(
-			(a, b) => Number(a.jahrgangsstufe.value) - Number(b.jahrgangsstufe.value)
-		);
-
-		const jahrgangsstufe = ['jahrgangsstufe', sortedJahrgangsstufe];
-		filters = [faecher, jahrgangsstufe];
-	}
-	onMount(() => {
-		loadFilters();
-	});
-
-	async function handleQuery(event) {
-		try {
-			event.preventDefault();
-			const res = await queryFTS(search);
-			results = await res.json();
-		} catch (error) {
-			console.error('Error fetching results:', error);
-		}
-	}
+	let searchTerm = '';
 </script>
 
 <div class="mt-2 flex w-full flex-col items-center justify-center">
-	<form onsubmit={handleQuery}>
+	<form onsubmit={() => handleQuery(searchTerm)}>
 		<div class="flex flex-col items-center justify-center md:flex md:flex-row">
 			<label for="query"
 				>Wonach suchst du?
-				<input bind:value={search} id="query" type="text" />
+				<input bind:value={searchTerm} id="query" type="text" />
 			</label>
 			<div>
-				{#if filters.length}
-					{#each filters as filter}
+				{#key $db.filters}
+					{#each $db.filters as filter}
 						<Filter {filter} />
 					{/each}
-				{/if}
+				{/key}
 			</div>
 		</div>
 	</form>
 
-	{#if results.length}
-		{#each results as result}
-			<Result {result} {search} />
+	{#if $db.results.length}
+		{#each $db.results as result}
+			<Result {result} search={searchTerm} />
 		{/each}
 	{/if}
 </div>
